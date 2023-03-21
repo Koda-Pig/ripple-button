e4c66d9d1951ec39b47b54154fd01f7b3e1acb5e
Josh Koter <josh.koter@gmail.com>
2023-03-21 12:13:27 +0200

    Finished ripple button

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ripple button effect
+
+Stopped [here](https://youtu.be/_bglQ-6T0r8?t=390).
--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('test')
+import './style.scss'
+
+const rippleBtn = document.querySelector('.ripple-btn')
+
+const mousePosToCustomProp = (event, elem) => {
+  let posX = event.offsetX,
+    posY = event.offsetY
+  elem.style.setProperty('--x', posX + 'px')
+  elem.style.setProperty('--y', posY + 'px')
+}
+
+rippleBtn.addEventListener('click', (e) => {
+  mousePosToCustomProp(e, e.target)
+  e.target.classList.add('pulse')
+  e.target.addEventListener('animationend', () => {
+    e.target.classList.remove('pulse')
+  })
+})
